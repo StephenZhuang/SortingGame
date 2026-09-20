bde92387a02fc512ff617c29f31abc74b6d06f52
Stephen <379128008@qq.com>
2026-09-20 15:53:38 +0800

    chore: 初始化 Xcode 项目结构

---
+++ SortingGame/SortingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingGameApp.swift
+//  SortingGame
+//
+//  Created by Stephen Zhuang on 2026/9/20.
+//
+
+import SwiftUI
+
+@main
+struct SortingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
