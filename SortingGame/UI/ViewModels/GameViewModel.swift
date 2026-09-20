91fc71ac0053a6322a6bc270ca7c8b01b760e65f
Stephen <379128008@qq.com>
2026-09-20 16:01:13 +0800

    feat: 添加 GameViewModel

---
+++ SortingGame/UI/ViewModels/GameViewModel.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import Observation
+
+@MainActor
+@Observable
+class GameViewModel {
+    let engine: GameEngine
+    var feedbackText: String = ""
+
+    init(bottleCount: Int) {
+        self.engine = GameEngine(bottleCount: bottleCount)
+        startNewGame()
+    }
+
+    func startNewGame() {
+        engine.startNewGame()
+        feedbackText = ""
+    }
+
+    func submitGuess() {
+        engine.submitGuess()
+        updateFeedback()
+    }
+
+    func giveUp() {
+        engine.giveUp()
+    }
+
+    private func updateFeedback() {
+        if case .playing = engine.state {
+            let correctCount = engine.correctPositionCount
+            feedbackText = "对了 \(correctCount) 个"
+        }
+    }
+}
