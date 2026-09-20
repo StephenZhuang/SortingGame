2681a3fc95e6483e908d0b8acdc9ed176f2851de
Stephen <379128008@qq.com>
2026-09-20 18:22:25 +0800

    feat: iOS target 接入共享游戏代码，删除未使用模板文件

--- SortingGame-iOS/SortingGame_iOSApp.swift
+++ SortingGame-iOS/SortingGame_iOSApp.swift
@@ -1,17 +1,10 @@
-//
-//  SortingGame_iOSApp.swift
-//  SortingGame-iOS
-//
-//  Created by Stephen Zhuang on 2026/9/20.
-//
-
 import SwiftUI
 
 @main
 struct SortingGame_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainMenuView()
         }
     }
 }
