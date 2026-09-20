ff33341e67f0703da84cf613c631fffbb72a1d7c
Stephen <379128008@qq.com>
2026-09-20 16:08:12 +0800

    fix: 降低 iOS 部署目标到 26.0 以匹配可用模拟器

---
+++ SortingGame-iOS/SortingGame_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingGame_iOSApp.swift
+//  SortingGame-iOS
+//
+//  Created by Stephen Zhuang on 2026/9/20.
+//
+
+import SwiftUI
+
+@main
+struct SortingGame_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
